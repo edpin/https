8b853d5c041bbad15adbcf2323f99f74ecd89a36
Eduardo Pinheiro <epinheiro@gmail.com>
2019-05-29 12:26:54 -0700

    adds preload directive to HSTS

--- server.go
+++ server.go
@@ -79,6 +79,6 @@ func NewHSTS(h http.Handler) http.Handler {
 
 // ServeHTTP implements http.Handler.
 func (h hstsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains")
+	w.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload")
 	h.h.ServeHTTP(w, r)
 }
